2cf7d96bddc71ef879eb8816575d9446cb691adb
Neil <74466880+spidernh@users.noreply.github.com>
2022-07-03 04:00:58 -0400

    Optimization
    - Started using ANSI escape codes to move the cursor to the top left instead of clearing the terminal (it's faster)
    - Moved everything into one print statement (might be slower but it's much simpler and still runs at 30FPS, so it's a nonissue)
    - Added something to limit the FPS to 30, so that it goes at real time (when making a video, it's easier to render when it's going at the correct speed and squashing/stretching it isn't needed)

--- main.py
+++ main.py
@@ -14,57 +14,49 @@ blank_before = int((console_resolution[0] - new_resolution[0]) / 2.0)
 frequency = (og_resolution[0] / new_resolution[0], og_resolution[1] / new_resolution[1])
 clear = lambda: os.system('cls')
 
+os.system('')
+new_frame_escape = f'\033[{new_resolution[0] + blank_before}D\033[{new_resolution[1]}A'
+new_line_escape = f'\033[{new_resolution[0] + blank_before}D'
+
 if not cap.isOpened():
     print('ERROR: VIDEO NOT OPENED')
 
 frame_counter = 0
 start_time = time.time()
-last_frame_time = time.time()
-lowest_fps = 1000
-highest_fps = 0
 while cap.isOpened():
     ret, frame = cap.read()
-    og_resolution = (len(frame), len(frame[0]))
-    ratio = og_resolution[0] / og_resolution[1]
-    new_resolution = (max(0, min(console_resolution[0], (round(2.73 * ratio * 29)))), 29)
-    frequency = (og_resolution[0] / new_resolution[0], og_resolution[1] / new_resolution[1])
-    frame_bw = frame
-    terminal_board = []
+
+    # Convert the big frame into a grid of pixels that can fit on the terminal 
+    terminal_grid = []
     for y in range(len(frame)):
         if floor(y % frequency[1]) == 0:
             row = []
             for x in range(len(frame[y])):
                 if floor(x % frequency[0]) == 0:
-                    frame_bw[y][x] = [255, 255, 255] if frame_bw[y][x][0] >= 128 else [0, 0, 0]
-                    row.append(frame_bw[y][x][0] == 255)
-            terminal_board.append(row)
-    clear()
-    print(f'size (tuple): {new_resolution[0]}x{new_resolution[1]}')
-    print(f'size (actual): {len(terminal_board[0])}x{len(terminal_board)}')
+                    frame[y][x] = [255, 255, 255] if frame[y][x][0] >= 128 else [0, 0, 0]
+                    row.append(frame[y][x][0] == 255)
+            terminal_grid.append(row)
 
+    # Draw in terminal
+    to_print = ''
     for y in range(new_resolution[1]):
         for i in range(blank_before):
-            print(' ', end='')
+            to_print += ' '
         for x in range(new_resolution[0]):
-            end = '\n' if x == new_resolution[0] - 1 else ''
-            ascii_char = '#' if terminal_board[y][x] else ' '
-            print(ascii_char, end=end)
+            to_print += '#' if terminal_grid[y][x] else ' '
+        to_print += new_line_escape
+    to_print += new_frame_escape
+    print(to_print)
 
     if ret == True:
-        # cv2.imshow('Bad Apple', frame_bw)
+        # cv2.imshow('Bad Apple', frame)
         if keyboard.is_pressed('q'):
             break
     else:
         break
 
     frame_counter += 1
-    fps = 1/(time.time() - last_frame_time)
-    if frame_counter > 5:
-        highest_fps = fps if fps > highest_fps else highest_fps
-        lowest_fps = fps if fps < lowest_fps else lowest_fps
-    # print(f'FPS BITCH: {fps}')
-    print(f'lowest: {lowest_fps}, highest: {highest_fps}')
-    time.sleep(0.06)
-    last_frame_time = time.time()
+    while time.time() - start_time < frame_counter * (1 / fps):
+        pass
 cap.release()
 cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import keyboard
+import os
 
 # for i in range(120, 122):
-for i in range(29):
-    for k in range(119):
-        if k != 118:
-            print('a', end='')
-        else:
-            print('a', end='\n')
+# for i in range(29):
+#     for k in range(119):
+#         if k != 118:
+#             print('a', end='')
+#         else:
+#             print('a', end='\n')
+os.system('')
+print('\033[31mbanana\033[m\033[1Dapple')
 
-while not keyboard.is_pressed('q'):
-    pass
+# while not keyboard.is_pressed('q'):
+#     pass
