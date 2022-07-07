9c1b7579a6de96f01733e4857db6eebeaa6f2a93
Neil <74466880+spidernh@users.noreply.github.com>
2022-07-06 22:08:30 -0400

    Made some changes
    I was trying to optimize this, I wasn't able to get it faster but there are still some changes.

--- ms-paint/main.py
+++ ms-paint/main.py
@@ -30,8 +30,11 @@ def move_at_screen(x: int, y: int):
 def move_at_canvas(x: int, y: int, offset: tuple):
     win32api.SetCursorPos((x + offset[0], y + offset[1]))
 
-def wait():
-    time.sleep(0.025)
+def wait_slow():
+    time.sleep(0.02)
+
+def wait_fast():
+    time.sleep(0.0001)
 
 def unglitch(res: tuple, offset: tuple):
     # Go through every 24 pixels to try and move the mouse everywhere to unglitch Paint
@@ -47,25 +50,19 @@ def unglitch(res: tuple, offset: tuple):
             if x % freq != 0:
                 continue
             move_at_canvas(x, y, offset)
-            time.sleep(0.0001)
+            wait_fast()
         move_at_canvas(res[0] - 1, y, offset)
-        time.sleep(0.0001)
+        wait_fast()
 
 def change_color(is_white: bool):
     if is_white:
         move_at_screen(white_position[0], white_position[1])
     else:
         move_at_screen(black_position[0], black_position[1])
-    wait()
+    wait_slow()
     click()
-    wait()
+    wait_slow()
     unclick()
-
-# Wait until P is pressed and un-pressed for the user to switch to Paint before starting.
-while not keyboard.is_pressed('p'):
-    wait()
-while keyboard.is_pressed('p'):
-    wait()
 
 # Set variables about the video and Paint
 video_resolution = (960, 720)
@@ -77,19 +74,28 @@ brush_position_2 = (330, 137)
 size_position_1 = (638, 67)
 size_position_2 = (667, 255)
 
+start_frame = 1884 # To know when to start saving frames (due to antialiasing making stuff weird)
+
+print(f'Ready to start on frame {start_frame - 4} to render {start_frame} and beyond.')
+
+# Wait until P is pressed and un-pressed for the user to switch to Paint before starting.
+while not keyboard.is_pressed('p'):
+    wait_slow()
+while keyboard.is_pressed('p'):
+    wait_slow()
+
 # Make sure Paint is setup
 click_screen(brush_position_1)
 time.sleep(1) # More waiting to allow for the menu to come up
 click_screen(brush_position_2)
-wait()
+wait_slow()
 click_screen(size_position_1)
 time.sleep(1) # More waiting to allow for the menu to come up
 click_screen(size_position_2)
-wait()
+wait_slow()
 click_screen(black_position)
-wait()
+wait_slow()
 # Set starting variables
-start_frame = 1724 # To know when to start saving frames (due to antialiasing making stuff weird)
 frame_counter = start_frame-4 # Frame counter to know what frame to render
 color_is_black = True # Starting color
 running = True # Loop variable
@@ -99,13 +105,13 @@ while running:
     frame = numpy.asarray(img)
     
     # Go through each row and draw it
-    freq = 8
+    brush_freq = 8
     for y in range(len(frame)):
-        if y % freq == 0:
+        if y % brush_freq == 0:
             # Go through each pixel in the row, check if the color is different from the section we're in.
             # If it is different, move the cursor and unclick, change colors, and click again.
             for x in range(len(frame[y])):
-                if x % freq == 0:
+                if x % brush_freq == 0:
                     # Check for pause
                     if keyboard.is_pressed('p'):
                         # Wait for p to stop being pressed, then wait for it to be pressed again (while also checking for quits)
@@ -129,7 +135,7 @@ while running:
                                 break
                         if running:
                             move_at_screen(position[0], position[1])
-                            wait()
+                            wait_slow()
                             click()
                         else:
                             break
@@ -144,15 +150,15 @@ while running:
                         if (frame[y][x][0] < 128) != color_is_black:
                             # Move cursor to draw line and unclick
                             move_at_canvas(x, y, canvas_position)
-                            wait()
+                            wait_slow()
                             unclick()
-                            wait()
+                            wait_slow()
                             # Swap colors
                             change_color(color_is_black)
                             color_is_black = not color_is_black
                             # Move cursor back and start drawing a new line
                             move_at_canvas(x, y, canvas_position)
-                            wait()
+                            wait_slow()
                             click()
                     else:
                         # Check the color of the frame, check the current color, and change if needed
@@ -162,21 +168,21 @@ while running:
                         # Start new row
                         move_at_canvas(x, y, canvas_position)
                         click()
-                        wait()
+                        wait_fast()
             move_at_canvas(len(frame[y]) - 1, y, canvas_position)
             unclick()
-            wait()
+            wait_slow()
         if not running:
             break
     unglitch(video_resolution, canvas_position)
-    wait()
+    wait_slow()
     if frame_counter >= start_frame:
         screenshot = pyautogui.screenshot()
         screenshot.save(f'render-frames/{frame_counter:04}.png')
     print(f'Finished frame {frame_counter:04} in {time.time() - last_frame_time} seconds.')
     frame_counter += 1
     last_frame_time = time.time()
-print(f'quit on y={y} x={x} black={color_is_black} position={position} frame={frame_counter}')
+print(f'quit on y={y} x={x} black={color_is_black} position={pyautogui.position()} frame={frame_counter}')
 print('press m to close')
 while not keyboard.is_pressed('m'):
     pass
